02367a1d57eb281179e0013746c3fae36a3a976e
Serhii Yatsenko <royalroad1995@gmail.com>
2020-05-28 20:00:08 +0300

    Add files via upload

---
+++ svpwm.c
@@ -0,0 +1,115 @@
+/**
+  ***********************************************************************************
+  * @file    svpwm.c
+  * @author  Serhii Yatsenko [royalroad1995@gmail.com]
+  * @version V1.0
+  * @date    May-2020
+  * @brief   This file provides firmware function for implementation the SVPWM - 
+  *			 		 Space-Vector Pulse Width Modulation for power electronics application
+  ***********************************************************************************
+  * @license
+  *
+  * MIT License
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in all
+  * copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
+  *
+  ***********************************************************************************
+  */
+
+/* Includes -----------------------------------------------------------------------*/
+#include "svpwm.h"
+
+/* Private typedef ----------------------------------------------------------------*/
+/* Private define -----------------------------------------------------------------*/
+#define M_Pi	3.1415926535897f	// float type of Pi constant value
+#define M_Pi_3	(M_Pi/3.0f)
+
+/* Private constants --------------------------------------------------------------*/
+
+/** 
+  * @brief	Matrix with cell indices in a time intervals table for SVPWM duty cycle
+  *					calculation technique
+  */ 
+const uint8_t au8PermuataionMatrix[6][3] =
+{	
+	{ 1, 2, 0 },
+	{ 3, 1, 0 },
+	{ 0, 1, 2 },
+	{ 0, 3, 1 },
+	{ 2, 0, 1 },
+	{ 1, 0, 3 }
+};
+
+/* Private macro ------------------------------------------------------------------*/
+/* Private variables --------------------------------------------------------------*/
+/* Private function prototypes ----------------------------------------------------*/
+/* Private functions --------------------------------------------------------------*/
+
+/**
+  * @brief  SVPWM calculation function. This function calculates three values of
+  *					duty cycle to generate a three-phase voltage vector with amplitude -
+  *					ptSVPWM->fUs and angle - ptSVPWM->fAngRad.
+  * @param  ptSVPWM: pointer to user data structure with type "tSVPWM".               
+  * @retval None
+  */
+void tSVPWM_calc(tSVPWM* ptSVPWM)
+{	
+	uint8_t	u8Sector;
+	float	fMaxUs, fScaledUs, fBeta, fTb1, fTb2, afTi[4];
+	
+	fMaxUs = ptSVPWM->fUdc * (1.0f/sqrtf(3.0f));
+	
+	switch(ptSVPWM->enInType)
+	{
+		case AlBe:
+			ptSVPWM->fUs = hypotf(ptSVPWM->fUbe, ptSVPWM->fUal);
+			ptSVPWM->fAngRad = atan2f(ptSVPWM->fUbe, ptSVPWM->fUal);
+		break;
+		
+		case UsAng:
+			ptSVPWM->fUs = fabsf(ptSVPWM->fUs);
+		break;
+		
+		default: return;
+	}
+	
+	if(ptSVPWM->fUs > fMaxUs) ptSVPWM->fUs = fMaxUs;
+	
+	fScaledUs = ptSVPWM->fUs/fMaxUs;
+	
+	ptSVPWM->fAngRad += M_Pi;
+	
+	u8Sector = (uint8_t)(ptSVPWM->fAngRad * (1.0f/M_Pi_3));
+	
+	fBeta = ptSVPWM->fAngRad - M_Pi_3 * u8Sector;
+	
+	fTb1 = fScaledUs * sinf(M_Pi_3 - fBeta);
+	fTb2 = fScaledUs * sinf(fBeta);
+	
+	afTi[0] = (1.0f - fTb1 - fTb2)*0.5f;
+	afTi[1] = fTb1 + fTb2 + afTi[0];
+	afTi[2] = fTb2 + afTi[0];
+	afTi[3] = fTb1 + afTi[0];
+		
+	ptSVPWM->fCCRA = ptSVPWM->fUdcCCRval * afTi[au8PermuataionMatrix[u8Sector][0]];
+	ptSVPWM->fCCRB = ptSVPWM->fUdcCCRval * afTi[au8PermuataionMatrix[u8Sector][1]];
+	ptSVPWM->fCCRC = ptSVPWM->fUdcCCRval * afTi[au8PermuataionMatrix[u8Sector][2]];
+}
+
+/*********************************** END OF FILE ***********************************/
---
+++ svpwm.h
@@ -0,0 +1,115 @@
+/**
+  ***********************************************************************************
+  * @file    svpwm.h
+  * @author  Serhii Yatsenko [royalroad1995@gmail.com]
+  * @version V1.0
+  * @date    May-2020
+  * @brief   This file contains the type definition of data structure and function
+  *			 		 prototypes for implementation the SVPWM - Space-Vector Pulse Width 
+  *					 Modulation
+  ***********************************************************************************
+  * @license
+  *
+  * MIT License
+  *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+  * of this software and associated documentation files (the "Software"), to deal
+  * in the Software without restriction, including without limitation the rights
+  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  * copies of the Software, and to permit persons to whom the Software is
+  * furnished to do so, subject to the following conditions:
+  *
+  * The above copyright notice and this permission notice shall be included in all
+  * copies or substantial portions of the Software.
+  *
+  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  * SOFTWARE.
+  *
+  ***********************************************************************************
+  */
+
+/* Define to prevent recursive inclusion ------------------------------------------*/
+#ifndef __SVPWM_H__
+#define __SVPWM_H__
+
+#ifdef __cplusplus
+	extern "C" {
+#endif
+
+/* Includes -----------------------------------------------------------------------*/
+#include <stdint.h>
+#include <math.h>
+
+/* Exported types -----------------------------------------------------------------*/
+
+/** 
+  * @brief	SVPWM module data input type
+  */
+typedef enum
+{
+	AlBe	= 0x00U,										// Alpha-Beta component input
+	UsAng	= 0x01U											// Magnitude-Angle component input
+} tInType;
+
+/** 
+  * @brief	"SVPWM Module" data structure
+  */
+typedef struct sSVPWM
+{
+// Inputs:
+	tInType	enInType;									// SVPWM module data input type
+	float	fUdc;												// DC Link voltage, Volts
+	float	fUdcCCRval;									// Counter compare register value which is
+																		// equivalent to full DC Link voltage
+	// enInType == AlBe:
+	float	fUal;												// Alpha input, Volts
+	float	fUbe;												// Beta input, Volts
+	// enInType == UsAng:
+	float	fUs;												// Magnitude input, Volts (enInType == UsAng)
+	float	fAngRad;										// Angle input, Rad (enInType == UsAng)
+// Outputs:
+	float	fCCRA;											// Counter compare register A value
+	float	fCCRB;											// Counter compare register B value
+	float	fCCRC;											// Counter compare register C value
+// Functions:
+	void	(*m_calc)(struct sSVPWM*);	// Pointer to SVPWM calculation function
+} tSVPWM;
+
+/* Exported constants -------------------------------------------------------------*/
+
+/** 
+  * @brief	Initialization constant with defaults for user variables
+  *			with "tSVPWM" type
+  */
+#define SVPWM_DEFAULTS {						\
+	.enInType		= AlBe,								\
+	.fUal				= 0.0f,								\
+	.fUbe				= 0.0f,								\
+	.fUs				= 0.0f,								\
+	.fAngRad		= 0.0f,								\
+	.fUdc				= 0.0f,								\
+	.fUdcCCRval	= 0.0f,								\
+	.fCCRA			= 0.0f,								\
+	.fCCRB			= 0.0f,								\
+	.fCCRC			= 0.0f,								\
+	.m_calc			= tSVPWM_calc					\
+}
+
+/* Exported macro -----------------------------------------------------------------*/
+/* Exported functions -------------------------------------------------------------*/
+
+/* SVPWM Duty Cycles calculation function prototype ********************************/
+void tSVPWM_calc(tSVPWM*);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __SVPWM_H__ */
+
+/*********************************** END OF FILE ***********************************/
